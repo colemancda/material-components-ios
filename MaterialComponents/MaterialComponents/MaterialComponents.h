51dc5c250ef44eb2dfb59deef329fb0b3ecb6315
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-02-02 16:04:42 -0500

    Added iOS dynamic framework

---
+++ MaterialComponents/MaterialComponents/MaterialComponents.h
@@ -0,0 +1,16 @@
+//
+//  MaterialComponents.h
+//  MaterialComponents
+//
+//  Created by Alsey Coleman Miller on 2/2/18.
+//  Copyright © 2018 Google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MaterialComponents.
+FOUNDATION_EXPORT double MaterialComponentsVersionNumber;
+
+//! Project version string for MaterialComponents.
+FOUNDATION_EXPORT const unsigned char MaterialComponentsVersionString[];
+
